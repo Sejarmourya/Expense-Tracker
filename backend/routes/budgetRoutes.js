a3ad81e56af5608a5ada4d9d67c868bb162638e9
Sejar_Mourya <sejarsinger@gmail.com>
2026-09-01 21:35:09 +0530

    fix model filename case

--- backend/routes/budgetRoutes.js
+++ backend/routes/budgetRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Expense = require("../models/Expense");
-const Budget = require("../models/Budget");
+const Budget = require("../models/budget");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
